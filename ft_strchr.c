2d302a3c9700799b716979f1ee8ad63043fbc29a
Isabelle REN <iren@e2r1p3.42.fr>
2020-09-01 20:12:53 +0200

    commentaires et autre enleves pour la normi

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,22 +6,9 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 14:35:20 by iren              #+#    #+#             */
-/*   Updated: 2020/09/01 00:12:19 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/01 20:08:07 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
-** strchr, strrchr, strchrnul - locate character in string
-** DESCRIPTION
-**  The strchr() function returns a pointer to the first occurrence of the
-**	character c in the string s.
-** RETURN VALUE
-**  The  strchr() function returns a pointer to the matched character or NULL
-**	if the character is not found.
-**  The terminating null byte is considered part of the string, so that
-**	if c is specified as '\0', these functions return a pointer
-**	to the terminator.
-*/
 
 #include "libft.h"
 
@@ -31,7 +18,7 @@ char	*ft_strchr(const char *s, int c)
 	char	*st;
 
 	i = 0;
-	st = (char *)s;
+	st = (char*)s;
 	while (st[i])
 	{
 		if (c == st[i])
@@ -40,5 +27,4 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (c == '\0')
 		return (&st[i]);
-	return (0);
 }
