8ebbff1267f183034ac65bab5383b9831da9218c
isabelle <iren42@student.42.fr>
2020-09-01 19:36:01 +0200

    commentaire sans tiret

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 13:27:20 by iren              #+#    #+#             */
-/*   Updated: 2020/08/31 23:25:01 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/01 19:17:28 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 **  checks for an alphabetic character; in the standard "C" locale, it is
 **	equivalent to (isupper(c) || islower(c)).
 **  In some locales, there may be additional characters  for  which  isalpha()
-**  is true—letters which are neither uppercase nor lowercase.
+**  is true letters which are neither uppercase nor lowercase.
 **
 ** RETURN VALUE
 **  The values returned are nonzero if the character c falls into the
