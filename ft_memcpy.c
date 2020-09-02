c0c60e775a5563cbe053324c8b2996550ebb182e
Isabelle REN <iren@e2r1p9.42.fr>
2020-09-03 01:53:36 +0200

    si src et dest sont NULL alors retourner NULL

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 18:14:53 by iren              #+#    #+#             */
-/*   Updated: 2020/09/03 01:46:00 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/03 01:51:57 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	s = (unsigned char*)src;
 	d = (unsigned char*)dest;
 	if (src == 0 && dest == 0)
-		return (0);
+		return (dest);
 	while (i < n)
 	{
 		d[i] = s[i];
