b478026ba3223e4f3a1c2b1b62f0ca8934bd5614
isabelle <iren42@student.42.fr>
2020-09-03 10:00:25 +0200

    full size of the buffer and s2 == NULL

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/08 23:25:57 by iren              #+#    #+#             */
-/*   Updated: 2020/09/03 00:51:09 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/03 09:58:14 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,12 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	unsigned int	len;
 
 	res = 0;
-	if (s1 != 0)
-	{
-		len = ft_strlen(s1) + ft_strlen(s2);
-		if (!(res = malloc(sizeof(char) * (len + 1))))
-			return (0);
-		ft_strlcpy(res, s1, ft_strlen(s1) + 1);
-		ft_strlcat(res, s2, len + 1);
-	}
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(res = malloc(sizeof(char) * (len + 1))))
+		return (0);
+	ft_strlcpy(res, s1, len + 1);
+	ft_strlcat(res, s2, len + 1);
 	return (res);
 }
