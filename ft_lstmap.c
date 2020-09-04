3c0289cf4f9cfb6accf510d004e64733ec7270b2
isabelle <iren42@student.42.fr>
2020-09-05 00:44:26 +0200

    bonus ajout

---
+++ ft_lstmap.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/05 00:17:36 by iren              #+#    #+#             */
+/*   Updated: 2020/09/05 00:38:53 by iren             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
+{
+	t_list			*res;
+
+	if (lst == 0)
+		return (0);
+	if (!(res = malloc(sizeof(t_list) * (ft_lstsize(lst)))))
+		return (0);
+	while (lst->next)
+	{
+		if ((*res = ft_lstnew(f(lst))) == 0)
+		{
+			ft_lstclear(res, del);
+			return (0);
+		}
+		lst = lst->next;
+		res = res->next;
+	}
+	return (res);
+}
