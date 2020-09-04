6c0d18afd9f96c7afe03f88e5f505360d7c3280c
isabelle <iren42@student.42.fr>
2020-09-05 00:49:32 +0200

    up

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/05 00:17:36 by iren              #+#    #+#             */
-/*   Updated: 2020/09/05 00:38:53 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/05 00:49:06 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void*))
 		return (0);
 	while (lst->next)
 	{
-		if ((*res = ft_lstnew(f(lst))) == 0)
+		if ((res = ft_lstnew(f(lst))) == 0)
 		{
-			ft_lstclear(res, del);
+			ft_lstclear(*res, del);
 			return (0);
 		}
 		lst = lst->next;
