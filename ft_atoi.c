c34d86e1ec84d92a5c8622b1651f48ad7e4aeec5
user42 <iren@student.42.fr>
2020-09-04 07:58:31 +0200

    over long min max OK

--- ft_atoi.c
+++ ft_atoi.c
@@ -19,8 +19,7 @@ int	ft_atoi(const char *s)
 	res = 0;
 	i = 0;
 	minus = 1;
-	while (s[i] == '\t' || s[i] == '\n' || s[i] == ' ' ||
-			s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
+	while ((s[i] >= '\t' && s[i] <= '\r') || s[i] == ' ')
 		i++;
 	if (s[i] == '+' || s[i] == '-')
 	{
@@ -28,10 +27,14 @@ int	ft_atoi(const char *s)
 			minus *= (-1);
 		i++;
 	}
-	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	while (s[i] && (s[i] >= '0' && s[i] <= '9') && !(res > 2147483648))
 	{
 		res = res * 10 + s[i] - '0';
 		i++;
 	}
+	if (res > 2147483647 && minus > 0)
+		return (-1);
+	if (res > 2147483648 && minus < 0)
+		return (0);
 	return ((int)minus * res);
 }
