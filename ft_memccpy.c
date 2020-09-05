8b81e6385c6d81bdbaa34a02f2238b53c1469162
isabelle <iren42@student.42.fr>
2020-09-05 03:03:23 +0200

    plus propre

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: iren <iren@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/14 17:01:19 by iren              #+#    #+#             */
-/*   Updated: 2020/09/04 21:10:51 by iren             ###   ########.fr       */
+/*   Updated: 2020/09/05 03:03:07 by iren             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,10 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	d = (unsigned char*)dest;
 	while (i < n)
 	{
-		if (s[i] == (unsigned char)c)
-		{
-			ft_memset(&d[i], (int)s[i], 1);
-			return ((void*)&d[++i]);
-		}
 		ft_memset(&d[i], (int)s[i], 1);
+		if (s[i] == (unsigned char)c)
+			return ((void*)&d[++i]);
 		i++;
 	}
-	if (i == n)
-		return (0);
-	return ((void*)&d[i]);
+	return (0);
 }
